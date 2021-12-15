5bc5d2b68d0a1132c86d99a4a9e1b880d574ac0c
yosuke-oka <y.oka711@gmail.com>
2021-12-15 17:49:52 +0900

    wip

--- arith/src/main.rs
+++ arith/src/main.rs
@@ -1,3 +1,31 @@
+//#![feature(box_syntax)]
+
+#[derive(Clone, Debug)]
+enum Term {
+    True,
+    False,
+    If(Box<Term>, Box<Term>, Box<Term>),
+    Zero,
+    Succ(Box<Term>),
+    Pred(Box<Term>),
+    IsZero(Box<Term>),
+}
+
+fn isNumericVal(t: &Term) -> bool{
+    match t {
+        Term::Zero => true,
+        Term::Succ(ref t1) => isNumericVal(t1),
+        _ => false
+    }
+}
+
+
+use Term::*;
+
 fn main() {
-    println!("Hello, world!");
+    let t = Zero;
+    println!("{:?}", isNumericVal(&t));
+
+    let t = Box::new(Box::new(Succ(Box::new(Succ(Box::new(Zero))))));
+    println!("{:?}", isNumericVal(&t));
 }
