a0754e6603e25d5daa2e6c5a4aebc1e748f5e8eb
yosuke-oka <y.oka711@gmail.com>
2022-03-13 18:57:23 +0900

    add type

--- simplebool/src/main.rs
+++ simplebool/src/main.rs
@@ -1,11 +1,14 @@
 #![feature(box_syntax, box_patterns)]
 //use std::fmt;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug)]
 enum Term {
     Var(usize, usize),
-    Abs(String, Box<Term>),
+    Abs(String, Type, Box<Term>),
     App(Box<Term>, Box<Term>),
+    True,
+    False,
+    If(Box<Term>, Box<Term>, Box<Term>),
 }
 
 #[derive(Clone, Debug)]
@@ -23,6 +26,7 @@ type Context = Vec<(String, Binding)>;
 
 use Binding::*;
 use Term::*;
+use Type::*;
 
 fn add_binding(ctx: &Context, x: &String, bind: Binding) -> Context {
     let mut new_ctx = ctx.clone();
@@ -47,9 +51,9 @@ fn print_term(ctx: &Context, t: &Term) {
                     panic!("bad index")
                 }
             }
-            Abs(x, t1) => {
+            Abs(x, t, t1) => {
                 let (new_ctx, new_x) = pickup_freshname(ctx, x);
-                print!("(λ {}. ", new_x);
+                print!("(λ {}:{:?}. ", new_x, t);
                 inner(&new_ctx, t1);
                 print!(")");
             }
@@ -59,6 +63,16 @@ fn print_term(ctx: &Context, t: &Term) {
                 print!(" ");
                 inner(&ctx, t2);
                 print!(")");
+            }
+            True => println!("True"),
+            False => println!("False"),
+            If(t1, t2, t3) => {
+                print!("If ");
+                inner(&ctx, t1);
+                print!(" then ");
+                inner(&ctx, t2);
+                print!(" else ");
+                inner(&ctx, t3);
             }
         }
     }
@@ -98,8 +112,15 @@ fn term_shift(d: isize, t: &Term) -> Term {
                     Var(*x, (*n as isize + env.d) as usize)
                 }
             }
-            Abs(x, t1) => Abs(x.clone(), box walk(env, c + 1, t1)),
+            Abs(x, t, t1) => Abs(x.clone(), t.clone(), box walk(env, c + 1, t1)),
             App(t1, t2) => App(box walk(env, c, t1), box walk(env, c, t2)),
+            True => t.clone(),
+            False => t.clone(),
+            If(t1, t2, t3) => If(
+                box walk(env, c, t1),
+                box walk(env, c, t2),
+                box walk(env, c, t3),
+            ),
         }
     }
 
@@ -121,8 +142,15 @@ fn term_subst(j: usize, s: &Term, t: &Term) -> Term {
                     Var(*x, *n)
                 }
             }
-            Abs(x, t1) => Abs(x.clone(), box walk(env, c + 1, t1)),
+            Abs(x, t, t1) => Abs(x.clone(), t.clone(), box walk(env, c + 1, t1)),
             App(t1, t2) => App(box walk(env, c, t1), box walk(env, c, t2)),
+            True => t.clone(),
+            False => t.clone(),
+            If(t1, t2, t3) => If(
+                box walk(env, c, t1),
+                box walk(env, c, t2),
+                box walk(env, c, t3),
+            ),
         }
     }
 
@@ -137,14 +165,14 @@ fn term_subst_top(s: &Term, t: &Term) -> Term {
 
 fn is_val(_ctx: &Context, t: &Term) -> bool {
     match t {
-        Abs(_, _) => true,
+        Abs(_, _, _) => true,
         _ => false,
     }
 }
 
 fn eval1(ctx: &Context, t: &Term) -> Term {
     match t {
-        App(box Abs(_x, t12), box v2) if is_val(ctx, v2) => term_subst_top(v2, t12),
+        App(box Abs(_x, _, t12), box v2) if is_val(ctx, v2) => term_subst_top(v2, t12),
         App(box v1, box t2) if is_val(ctx, v1) => App(box v1.clone(), box eval1(ctx, t2)),
         App(box t1, box t2) => App(box eval1(ctx, t1), box t2.clone()),
         _ => panic!("NoRuleApplies"),
@@ -160,25 +188,24 @@ fn eval(ctx: &Context, t: &Term) -> Term {
 }
 
 fn main() {
-    let ctx = vec![("x".to_string(), NameBind)];
-    let var = Var(0, 1);
-    print_term(&ctx, &var);
-
-    let abs = Abs("x".to_string(), box Var(1, 2));
-    print_term(&ctx, &abs);
-
-    let app = App(box abs, box var);
-    print_term(&ctx, &app);
-
     let ctx = vec![];
-    let tru = Abs("t".to_string(), box Abs("f".to_string(), box Var(0, 2)));
-    let fls = Abs("t".to_string(), box Abs("f".to_string(), box Var(1, 2)));
+    let tru = Abs(
+        "t".to_string(),
+        Bool,
+        box Abs("f".to_string(), Bool, box Var(0, 2)),
+    );
+    let fls = Abs(
+        "t".to_string(),
+        Bool,
+        box Abs("f".to_string(), Bool, box Var(1, 2)),
+    );
     print_term(&ctx, &tru);
     print_term(&ctx, &fls);
     let and = App(
         box Abs(
             "b".to_string(),
-            box Abs("c".to_string(), box App(box Var(0, 2), box Var(1, 2))),
+            Bool,
+            box Abs("c".to_string(), Bool, box App(box Var(0, 2), box Var(1, 2))),
         ),
         box fls.clone(),
     );
